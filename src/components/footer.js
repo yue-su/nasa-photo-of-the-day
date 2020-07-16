dc936e1b3a7488d997183fc55851ba51ac250ad3
Yue Su <yuesu@Mac-Pro.local>
2020-07-16 19:32:38 -0300

    adding icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from "react"
 import "../App.css"
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const StyledFooter = styled.div`
   width: 800px;
@@ -8,15 +10,29 @@ const StyledFooter = styled.div`
   font-size: 120%;
   margin: 0 auto;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 5rem;
+  height: 4rem;
+  padding-bottom: 2rem;
+
+  .footer__icon {
+    font-size: 200%;
+    margin-bottom: 1rem;
+  }
 `
 
 function Footer(props) {
   return (
     <StyledFooter>
       <h3>Made by Yue</h3>
+      <a
+        href="https://github.com/yue-su/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} className="footer__icon" />
+      </a>
     </StyledFooter>
   )
 }
